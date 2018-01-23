3a9e6933475e63cf034416a8a7f626baa793c989
Shruti Rachh <shrutirachh@shrutis-mbp.attlocal.net>
2018-01-22 22:00:35 -0600

    scraped images of different cities such as rome, paris and new york

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 '''Flickr keys and params'''
 FLICKR_PUBLIC = 'YOUR FLICKR PUBLIC KEY'
 FLICKR_SECRET = 'YOUR FLICKR PRIVATE KEY'
-search_text = 'paris'
-extras = 'url_m,geo'
-photos_per_page = 500
 
 '''DB params'''
 db_name = 'test.db'
